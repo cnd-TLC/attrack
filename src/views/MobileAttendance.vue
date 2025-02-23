403544c82990d34bcb0440e5598d7e6f4ffdfb86
Silferion <dizacn.prsn@gmail.com>
2025-02-23 23:07:08 +0800

    testing #2

--- src/views/MobileAttendance.vue
+++ src/views/MobileAttendance.vue
@@ -19,8 +19,8 @@
         <div class="clear-inside"></div>
       </div>
       
-      <canvas ref="canvasElement" style="display: none;"></canvas>
     </div>
+    <canvas ref="canvasElement" style="display: none;"></canvas>
 
     <!-- Capture Info and Buttons -->
     <div class="capture-info">
@@ -167,8 +167,8 @@
   // Get the center rectangle dimensions
   const rectWidth = canvas.width * 0.5;
   const rectHeight = canvas.height * 0.1;
-  const rectX = canvas.width;
-  const rectY = canvas.height;
+  const rectX = (canvas.width - rectWidth) / 2;
+  const rectY = (canvas.height - rectHeight) / 2;
 
   // Draw only the center rectangle on a temporary canvas
   const tempCanvas = document.createElement('canvas');
